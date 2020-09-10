b891b738181b8aacc8d407d3feb5c936aab8aaf2
hojmax <axelhojmark@gmail.com>
2020-09-10 14:26:01 +0200

    Create Problem 2.fsx

---
+++ Problem 2.fsx
@@ -0,0 +1,12 @@
+let mutable a = 1
+let mutable b = 0
+let mutable result = 0
+
+while a + b < int 4e6 do
+  let hold = a
+  a <- a+b
+  b <- hold
+  if a % 2 = 0 then
+    result <- result + a
+
+printfn "%A" result
