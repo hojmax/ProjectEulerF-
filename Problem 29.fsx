808667891d3ae2b3b0528b3310ffd4fc3f76293e
hojmax <axelhojmark@gmail.com>
2020-09-15 00:28:00 +0200

    Create Problem 29.fsx

---
+++ Problem 29.fsx
@@ -0,0 +1,14 @@
+open System
+let mutable possible = Set.empty
+
+let iPown a b =
+  let mutable result = 1I
+  for i in 1I..b do
+    result <- result * a
+  result
+
+for a in 2I..100I do
+  for b in 2I..100I do
+    possible <- possible.Add(iPown a b)
+
+printfn "%A" possible.Count
