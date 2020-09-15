a0a1eeb62b5830a52cb72a6693be419f98723b7a
hojmax <axelhojmark@gmail.com>
2020-09-15 14:39:32 +0200

    Create Problem 34.fsx

---
+++ Problem 34.fsx
@@ -0,0 +1,23 @@
+let factorial number =
+  let mutable total = 1
+  for i in 2..number do
+    total <- total * i
+  total
+
+let preComputedFactorials = [|for i in 0..9 -> factorial i|]
+
+let digitFactorial number =
+  let mutable n = number
+  let mutable total = 0
+  while n >= 1 do
+    total <- total + preComputedFactorials.[n % 10]
+    n <- n / 10
+  total
+
+let mutable total = 0
+// Kan max være 7-cifrede tal, da 8 * 9! = 2903040, hvilket er 7-cifret
+for i in 3..9999999 do
+  if digitFactorial i = i then
+    total <- total + i
+
+printfn "%A" total
