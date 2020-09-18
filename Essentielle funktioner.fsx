64d8f71b572e17569c31164e488f9e18c77fa2ff
hojmax <axelhojmark@gmail.com>
2020-09-18 23:03:02 +0200

    Update Essentielle funktioner.fsx

--- Essentielle funktioner.fsx
+++ Essentielle funktioner.fsx
@@ -11,6 +11,21 @@ let primeFactors number =
     result <- n :: result
   result
 
+let divisorAmount number =
+  let mutable n = number
+  let mutable amount = 1
+  let mutable i = 2
+  while i <= int (sqrt (float n)) do
+    let mutable power = 0
+    while n % i = 0 do
+      power <- power + 1
+      n <- n / i
+    amount <- amount * (power + 1)
+    i <- i + 1
+  if n <> 1 then
+    amount <- amount * 2
+  amount
+
 // Kan udvides til at kunne håndtere større tal
 let digitAmount n =
   let mutable i = n
