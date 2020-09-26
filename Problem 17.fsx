bebe1ef9f7cbe7ac358cb1552dd15386c66e821c
hojmax <axelhojmark@gmail.com>
2020-09-26 15:32:22 +0200

    Update Problem 17.fsx

--- Problem 17.fsx
+++ Problem 17.fsx
@@ -6,6 +6,17 @@ let numberLetters = Map.empty.Add(0, 0).Add(1, 3).Add(2, 3).Add(3, 5).Add(4, 4)
                              .Add(700, 12).Add(800, 12).Add(900, 11).Add(1000, 11)
 let deltaTeens = Map.empty.Add(14,1).Add(16,1).Add(17,1).Add(19,1)
 
+let digitAmount n =
+  let mutable i = n
+  let mutable result = 1
+  if i >= 100 then
+    result <- result + 2
+    i <- i / 100
+  if i >= 10 then
+    result <- result + 1
+    i <- i / 10
+  result
+
 let numberToLetters number =
   // Specialtilfælde "and"
   let mutable letterCount = if number >= 100 && number % 100 <> 0 then 3 else 0
